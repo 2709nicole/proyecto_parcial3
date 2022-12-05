b385d7c64ff980c8511f959e2db86538f9a4720f
2709nicole <99852409+2709nicole@users.noreply.github.com>
2022-12-04 19:32:45 -0600

    Add files via upload

---
+++ proyectovideojuegos_parcial3/inicio.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>video gamestore</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/all.css">
+    <script src="js/jquery-3.6.1.js"></script>
+</head>
+<body style="background-color: #597EAA;">
+    <?php include 'menu.php'?>
+    <br>
+        <nav class="nav" style='background-color: #E06666;'>
+            <p class="">Hot sale!!!Hot sale!!!Hot sale!!!Hot sale!!!Hot sale!!!Hot sale!!!Hot sale!!!Hot sale!!!
+            Hot sale!!!Hot sale!!!Hot sale!!!Hot sale!!!Hot sale!!!Hot sale!!!Hot sale!!!Hot sale!!!Hot sale!!!Hot sale!!!
+            Hot sale!!!Hot sale!!!Hot sale!!!
+            </p>
+        </nav>
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
---
+++ proyectovideojuegos_parcial3/menu.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #085394;">
+        <button style="background-color: #085394;"><i class="fas fa-bars" ></i></button>
+        <div class="container">
+        <a class="navbar-brand" href="inicio.php">Video gamestore</a>
+        </div>
+      
+        <div class="row">
+          <form class="form-inline my-3 my-lg-0" method="GET" action="resultadoBusqueda.php">
+            <input name="termino" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          </form>
+        </div>
+        <div class="container">
+        <i class="">Registrar</i>
+        <a class="btn"href="registrar.php"><i class="fas fa-sticky-note"></i></a>
+        </div>
+        <div class="container">
+          <a class="btn"href="carrito.php"><i class="fas fa-shopping-cart"></i></a>
+        </div>
+        <div class="container">
+        <i class="">Login</i>
+        <a class="btn"href="login.php"><i class="fas fa-user"></i></a>
+        </div>
+</nav>
